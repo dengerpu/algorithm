24de41a93625feba7f4264a6e932fc2ef636c851
dep <1649743146@qq.com>
2022-10-31 14:48:50 +0800

    feat: 寻找发帖水王

--- src/main/java/com/ep/example/exercise11.java
+++ src/main/java/com/ep/example/exercise11.java
@@ -17,11 +17,13 @@ import java.util.Set;
  */
 public class exercise11 {
     public static void main(String[] args) {
-        int []arr= {1,5,2,3,2,2,2};
+//        int []arr= {1,5,2,3,2,2,2};
+        int []arr= {1,2,1,3,1,4,1};
         System.out.println(f(arr));
         System.out.println(f2(arr));
         System.out.println(f3(arr));
         System.out.println(f4(arr));
+        System.out.println(f5(arr));
     }
     // 解法1:排序后返回arr[N/2]
     static int f(int[] arr) {
@@ -76,5 +78,42 @@ public class exercise11 {
             }
         }
         return candidate;
+    }
+
+    /**
+     * //变化，出现次数恰好为个数的一半，求出这个数
+     * 关于加强版水王的题我有个想法可以扫描一遍数组就解决问题：
+     * 水王占总数的一半，说明总数必为偶数；
+     * 不失一般性，假设隔一个数就是水王的id，两两不同最后一定会消减为0
+     * 水王可能是最后一个元素，每次扫描的时候，多一个动作，和最后一个元素做比较，单独计数，计数恰好等于一半
+     * 如果不是，计数不足一半，那么去掉最后一个元素，水王就是留下的那个candidate
+     * */
+    static int f5(int arr[]) {
+        // 候选数
+        int candidate = arr[0];
+        int nTimes = 0;
+        int countOfLast = 0; // 统计最后这个元素出现的次数
+        int N = arr.length;
+
+        for (int i = 0; i < N; i++) {
+            // 增加和最后一个元素比较的步骤
+            if(arr[i] == arr[N-1]) countOfLast++;
+            if(nTimes == 0) {
+                candidate = arr[i];
+                nTimes++;
+                continue;
+            }
+            if(arr[i] == candidate) {
+                nTimes++;
+            }else {
+                nTimes--;
+            }
+        }
+        // 最后一个元素出现的次数是n/2
+        if(countOfLast == N/2 ){
+           return arr[N-1];
+        }else {
+           return candidate;
+        }
     }
 }
