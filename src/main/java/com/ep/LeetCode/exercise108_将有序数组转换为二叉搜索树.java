07b153398b439e7b9a5cd227040859fb51af1ac5
dep <1649743146@qq.com>
2022-11-22 11:17:14 +0800

    feat: 将有序数组转换为二叉搜索树

---
+++ src/main/java/com/ep/LeetCode/exercise108_将有序数组转换为二叉搜索树.java
@@ -0,0 +1,36 @@
+package com.ep.LeetCode;
+
+/***
+ * @author dep
+ * @version 1.0
+ * @date 2022-11-22 10:14
+ */
+public class exercise108_将有序数组转换为二叉搜索树 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return helper(nums, 0, nums.length - 1);
+    }
+    public TreeNode helper(int[] nums, int left, int right) {
+        if (left > right) {
+            return null;
+        }
+        // 总是选择中间位置左边的数字作为根节点
+        int mid = (left + right) / 2;
+
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = helper(nums, left, mid - 1);
+        root.right = helper(nums, mid+1,right);
+        return root;
+    }
+}
