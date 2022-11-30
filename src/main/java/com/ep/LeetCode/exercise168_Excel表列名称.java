5be11d7ec945c9ee448b64cca1f54aa2fd067398
dep <1649743146@qq.com>
2022-11-30 12:11:58 +0800

    feat: excel表列名称

---
+++ src/main/java/com/ep/LeetCode/exercise168_Excel表列名称.java
@@ -0,0 +1,34 @@
+package com.ep.LeetCode;
+
+/***
+ * @author dep
+ * @version 1.0
+ * @date 2022-11-30 11:20
+ */
+public class exercise168_Excel表列名称 {
+    public static String convertToTitle(int columnNumber) {
+        StringBuffer stringBuffer = new StringBuffer();
+        while (columnNumber != 0) {
+            columnNumber --;
+            stringBuffer.append((char)((columnNumber % 26) + 'A'));
+            columnNumber /= 26;
+        }
+        return stringBuffer.reverse().toString();
+    }
+    
+    public static String convertTotitle(int columnNumber) {
+        StringBuffer stringBuffer = new StringBuffer();
+        while (columnNumber > 0) {
+            int a0 = (columnNumber - 1) % 26 + 1;
+            stringBuffer.append((char)(a0-1+'A'));
+            columnNumber = (columnNumber - a0) / 26;
+         }
+        return stringBuffer.reverse().toString();
+    }
+
+
+    public static void main(String[] args) {
+        int num = 701;
+        System.out.println(convertTotitle(num));
+    }
+}
