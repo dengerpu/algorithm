9da0a31fd38af6a45ef29407ae6d3fe2d3d5a146
dep <1649743146@qq.com>
2022-12-10 14:06:08 +0800

    feat: 用队列实现栈

--- src/main/java/com/ep/LeetCode/exercise225_用队列实现栈.java
+++ src/main/java/com/ep/LeetCode/exercise225_用队列实现栈.java
@@ -43,6 +43,29 @@ public class exercise225_用队列实现栈 {
 
     // 方法二 一个队列
     class MyStack2 {
-       /
+        Queue<Integer> queue;
+        public MyStack2() {
+            queue = new LinkedList<>();
+        }
+
+        public void push(int x) {
+            int n = queue.size();
+            queue.offer(x);
+            for (int i = 0; i < n; i++) {
+                queue.offer(queue.poll());
+            }
+        }
+
+        public int pop() {
+            return queue.poll();
+        }
+
+        public int top() {
+            return queue.peek();
+        }
+
+        public boolean empty() {
+            return queue.isEmpty();
+        }
     }
 }
