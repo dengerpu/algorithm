6a3ed57d1aaf2c9d251d782cfd59f3835850bf9d
dep <1649743146@qq.com>
2022-12-17 12:08:24 +0800

    feat: 丑数

---
+++ src/main/java/com/ep/LeetCode/exercise263_丑数.java
@@ -0,0 +1,22 @@
+package com.ep.LeetCode;
+
+/***
+ * @author dep
+ * @version 1.0
+ * @date 2022-12-17 11:43
+ */
+public class exercise263_丑数 {
+    public boolean isUgly(int n) {
+        if( n <= 0 ) {
+            return false;
+        }
+        int[] factors = {2,3,5};
+        for (int factor : factors) {
+            while (n % factor == 0) {
+                n /= factor;
+            }
+        }
+        return n == 1;
+    }
+
+}
