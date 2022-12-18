7df37e287398e41a03f079e606b5e42d26f129fb
dep <1649743146@qq.com>
2022-12-18 22:37:44 +0800

    feat: 移动的0

--- src/main/java/com/ep/LeetCode/exercise283_移动0.java
+++ src/main/java/com/ep/LeetCode/exercise283_移动0.java
@@ -33,6 +33,23 @@ public class exercise283_移动0 {
         return true;
     }
 
+    // 答案解法
+    public void moveZeroes2(int[] nums) {
+        int n = nums.length, left = 0, right = 0;
+        while (right < n) {
+            if (nums[right] != 0) {
+                swap(nums,left, right);
+                left++;
+            }
+            right++;
+        }
+    }
+    public void swap(int[] nums, int left, int right) {
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
+    }
+
     public static void main(String[] args) {
         int[] nums = {0,1};
         moveZeroes(nums);
