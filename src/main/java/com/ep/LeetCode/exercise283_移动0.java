9e4c85447c0ea724b8129af36fbb991fe1a08c39
dep <1649743146@qq.com>
2022-12-18 22:27:50 +0800

    feat: 移动的0

---
+++ src/main/java/com/ep/LeetCode/exercise283_移动0.java
@@ -0,0 +1,41 @@
+package com.ep.LeetCode;
+
+import java.util.Arrays;
+
+/***
+ * @author dep
+ * @version 1.0
+ * @date 2022-12-18 21:53
+ */
+public class exercise283_移动0 {
+    public static void moveZeroes(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n-1; i++) {
+            if (nums[i] == 0) {
+                if (!IsZero(nums,i)) { // 如果后面不是全都都是零
+                    int t = nums[i];
+                    for (int j = i; j < n -1 ; j++) {
+                        nums[j] = nums[j+1];
+                    }
+                    nums[n-1] = t;
+                    i = -1;  // 需要重头再开始
+                }
+            }
+        }
+    }
+    // 判断是不是全为0
+    public static Boolean IsZero(int[] nums, int start) {
+        for (int i = start; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0,1};
+        moveZeroes(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+}
