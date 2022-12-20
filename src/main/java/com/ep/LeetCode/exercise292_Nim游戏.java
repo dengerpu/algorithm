34493b1b149e2521f7414b13bc71e2dfaefcae2e
dep <1649743146@qq.com>
2022-12-20 11:48:28 +0800

    feat: Nim游戏

---
+++ src/main/java/com/ep/LeetCode/exercise292_Nim游戏.java
@@ -0,0 +1,12 @@
+package com.ep.LeetCode;
+
+/***
+ * @author dep
+ * @version 1.0
+ * @date 2022-12-20 11:35
+ */
+public class exercise292_Nim游戏 {
+    public boolean canWinNim(int n) {
+        return n % 4 != 0;
+    }
+}
