6cfb38e6e324f8467bf8e4b5f431451f9a4895db
dep <1649743146@qq.com>
2022-12-20 11:28:34 +0800

    feat: 丢失的数字

--- src/main/java/com/ep/LeetCode/exercise268_丢失的数字.java
+++ src/main/java/com/ep/LeetCode/exercise268_丢失的数字.java
@@ -2,6 +2,7 @@ package com.ep.LeetCode;
 
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 
 /***
  * @author dep
@@ -34,6 +35,37 @@ public class exercise268_丢失的数字 {
         }
         return n;
     }
+
+    // 方法二 哈希表
+    public int missingNumber3(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            set.add(nums[i]);
+        }
+        int missing = -1;
+        for (int i = 0; i <= n; i++) {
+            if (!set.contains(i)) {
+                missing = i;
+                break;
+            }
+        }
+        return  missing;
+    }
+
+    // 方法三 位运算
+    public int missingNumber4(int[] nums) {
+        int xor = 0;
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            xor ^= nums[i];
+        }
+        for (int i = 0; i <=n ; i++) {
+            xor ^= i;
+        }
+        return xor;
+    }
+
 
     public static void main(String[] args) {
         int[] nums = {8,6,4,2,3,5,7,0,1};
