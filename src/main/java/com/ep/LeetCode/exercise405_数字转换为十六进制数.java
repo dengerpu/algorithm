e089324d86a0a4721bf203808ed4b42323ffa176
dep <1649743146@qq.com>
2022-12-30 12:26:02 +0800

    feat: 405.数字转换为十六进制数

--- src/main/java/com/ep/LeetCode/exercise405_数字转换为十六进制数.java
+++ src/main/java/com/ep/LeetCode/exercise405_数字转换为十六进制数.java
@@ -17,7 +17,7 @@ public class exercise405_数字转换为十六进制数 {
         for (int i = 7; i >= 0; i--) {
             // 0xf=0000 0000 0000 0000 0000 0000 0000 1111
             int val = (num >> (4*i)) & 0xf; // 可以得到二进制的前四位，也就是十六进制的第一位
-            if (sb.length() > 0 || val > 0) {
+            if (sb.length() > 0 || val > 0) {  // 为了保证前缀不为0
                 char digit = val < 10 ? (char) ('0' + val) : (char) ('a' + val - 10);
                 sb.append(digit);
             }
