b86c8c3b3a00f751753f423975a40f99c639297d
dep <1649743146@qq.com>
2023-01-06 19:32:56 +0800

    feat: 459.重复的子字符串(kmp算法)

--- src/main/java/com/ep/LeetCode/exercise459_重复的子字符串.java
+++ src/main/java/com/ep/LeetCode/exercise459_重复的子字符串.java
@@ -1,5 +1,7 @@
 package com.ep.LeetCode;
 
+import java.util.Arrays;
+
 /***
  * @author dep
  * @version 1.0
@@ -30,6 +32,40 @@ public class exercise459_重复的子字符串 {
     public boolean repeatedSubstringPattern2(String s) {
         // 例如  abcabc   s+s = abcabcabcabc 查找第一个s出现的索引位置不应该是s的长度的位置
         return (s+s).indexOf(s,1) != s.length();
+    }
+
+    // 方法三KMP算法
+    public boolean repeatedSubstringPattern3(String s) {
+        return kmp(s+s, s);
+    }
+
+    public boolean kmp(String query, String pattern) {
+        int n = query.length();
+        int m = pattern.length();
+        int[] fail = new int[m];
+        Arrays.fill(fail, -1);
+        for (int i = 1; i < m; i++) {
+            int j = fail[i-1];
+            while (j != -1 && pattern.charAt(j+1) != pattern.charAt(i)) {
+                j = fail[j];
+            }
+            if (pattern.charAt(j+1) == pattern.charAt(i)) {
+                fail[i] = j + 1;
+            }
+        }
+        int match = -1;
+        for (int i = 0; i < n-1; i++) {
+            while (match != -1 && pattern.charAt(match + 1) != query.charAt(i)) {
+                match = fail[match];
+            }
+            if (pattern.charAt(match + 1) == query.charAt(i)) {
+                ++match;
+                if (match == m - 1) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
 }
