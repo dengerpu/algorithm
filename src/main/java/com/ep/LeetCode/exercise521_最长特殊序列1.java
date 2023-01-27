307813f600b994cc4a435763975a756ec60bc3b6
dep <1649743146@qq.com>
2023-01-27 08:39:18 +0800

    feat: 521.最长特殊序列1

--- src/main/java/com/ep/LeetCode/exercise521_最长特殊序列1.java
+++ src/main/java/com/ep/LeetCode/exercise521_最长特殊序列1.java
@@ -7,6 +7,6 @@ package com.ep.LeetCode;
  */
 public class exercise521_最长特殊序列1 {
     public int findLUSlength(String a, String b) {
-
+        return !a.equals(b) ? Math.max(a.length(), b.length()) : -1;
     }
 }
