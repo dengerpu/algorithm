512f2edb7280308123cc517215743a7beecb9fcc
dep <1649743146@qq.com>
2023-02-21 13:04:34 +0800

    feat: 703.数据流中的第K大元素

---
+++ src/main/java/com/ep/LeetCode/exercise703_数据流中的第K大元素.java
@@ -0,0 +1,30 @@
+package com.ep.LeetCode;
+
+import java.util.PriorityQueue;
+
+/***
+ * @author dep
+ * @version 1.0
+ * @date 2023-02-21 11:10
+ */
+public class exercise703_数据流中的第K大元素 {
+    class KthLargest{
+        PriorityQueue<Integer> pq;
+        int k;
+        public KthLargest(int k, int[] nums) {
+                this.k = k;
+                pq = new PriorityQueue<Integer>();
+                for(int x: nums) {
+                    add(x);
+                }
+        }
+        public int add(int val) {
+            pq.offer(val);
+            if (pq.size() > k) {
+                pq.poll();
+            }
+            return pq.peek();
+        }
+    }
+
+}
