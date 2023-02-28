38c965192fa25b256d5121118661e6384aa48b2a
dep <1649743146@qq.com>
2023-02-28 20:19:47 +0800

    feat: 二分查找专题——34在排序数组中查找元素的第一个和最后一个位置

--- src/main/java/com/ep/LeetCode_Type/Array/exercise3_34_在排序数组中查找元素的第一个和最后一个位置.java
+++ src/main/java/com/ep/LeetCode_Type/Array/exercise3_34_在排序数组中查找元素的第一个和最后一个位置.java
@@ -59,4 +59,29 @@ public class exercise3_34_在排序数组中查找元素的第一个和最后一
         int target = 8;
         System.out.println(Arrays.toString(searchRange(nums, target)));
     }
+
+
+    public int[] searchRange2(int[] nums, int target) {
+        int leftIdx = binarySearch(nums, target, true);
+        int rightIdx = binarySearch(nums, target, false) - 1;
+        if (leftIdx <= rightIdx && rightIdx < nums.length && nums[leftIdx] == target && nums[rightIdx]== target) {
+            return new int[] {leftIdx, rightIdx};
+        }
+        return new int[] {-1, -1};
+    }
+
+    // 解法二
+    public int binarySearch(int[] nums, int target, boolean lower) {
+        int left = 0, right = nums.length - 1, ans = nums.length;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] > target || (lower && nums[mid] >= target)) {
+                right = mid - 1;
+                ans = mid;
+            }else {
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
 }
