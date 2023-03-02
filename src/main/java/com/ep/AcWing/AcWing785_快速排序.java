2bd5e58135e344d9af17e6f626213fa1f200d85d
dep <1649743146@qq.com>
2023-03-02 14:14:25 +0800

    feat: 785.快速排序

--- src/main/java/com/ep/AcWing/AcWing785_快速排序.java
+++ src/main/java/com/ep/AcWing/AcWing785_快速排序.java
@@ -61,23 +61,73 @@ public class AcWing785_快速排序 {
         }
     }
     // 平均复杂度o(nlogn)
-    public static void quick_sort2(int[] arr, int left, int right) {
-        if (left >= right) return;
-        int basic = arr[left +right >> 1], i = left, j = right;
-        while (i < j) {
-            while (i < j && basic < arr[j]) { // 操作右指针找到小于基数的下标
-                j--;
-            }
-            while (i < j && basic > arr[i]) { // 操作左指针找到大于基数的下标
+    public static void quick_sort2(int[] arr, int l, int r) {
+        if(l >= r){
+            return;
+        }
+        int x = arr[l + r >> 1],i = l - 1,j = r + 1;
+        while(i < j){
+            do{
                 i++;
-            }
-            if (i < j) {
+            }while(arr[i] < x);
+            do{
+                j--;
+            }while(arr[j] > x);
+            if(i < j){
                 int t = arr[i];
                 arr[i] = arr[j];
                 arr[j] = t;
             }
         }
-        quick_sort2(arr, left, j - 1);
-        quick_sort2(arr, j + 1, right);
+        quick_sort(arr,l,j);
+        quick_sort(arr,j+1,r);
+    }
+    public static void quickSort(int[] arr, int l, int r){
+        if (l < r) {
+            int q = partiotion(arr,l, r);
+            quickSort(arr,l,q-1); // 左侧快速排序
+            quickSort(arr,q+1,r); // 右侧快速排序
+        }
+    }
+    public static int partiotion(int[] arr, int l, int r) {
+        int pivot = arr[l];
+        int left = l + 1; // 左指针
+        int right = r; // 右指针
+        while (left <= right) {
+            if (left <= right && arr[left] <= pivot) left++;
+            if (left <= right && arr[right] > pivot) right--;
+            if (left < right) {
+                swap(arr,left,right);
+            }
+        }
+        // 循环退出时，right指向的最后一个小于等于主元的位置，也就是主元应该呆的位置
+        swap(arr, l, right);
+        return right;
+    }
+    /***
+     * 交换数组的值
+     * @param arr
+     * @param m
+     * @param n
+     */
+    public static void swap (int[] arr, int m , int n) {
+        int num = arr[m];
+        arr[m] = arr[n];
+        arr[n] = num;
+    }
+
+    /***
+     * 打印数组
+     * @param arr
+     */
+    public static void print(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            if (i != n-1) {
+                System.out.print(arr[i] + " ");
+            }else {
+                System.out.println(arr[i]);
+            }
+        }
     }
 }
