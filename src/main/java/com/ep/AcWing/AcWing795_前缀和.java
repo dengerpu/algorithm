9abebb209bf91135f3db26761d65e47553ad3a6c
dep <1649743146@qq.com>
2023-03-11 09:30:22 +0800

    feat: 795.前缀和

---
+++ src/main/java/com/ep/AcWing/AcWing795_前缀和.java
@@ -0,0 +1,36 @@
+package com.ep.AcWing;
+
+import java.util.Scanner;
+
+/***
+ * @author dep
+ * @version 1.0
+ * @date 2023-03-10 22:31
+ */
+public class AcWing795_前缀和 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        int [] sums = PrefixSum(arr,n);
+        while (m!= 0) {
+            int l = scanner.nextInt();
+            int r = scanner.nextInt();
+            System.out.println(sums[r] - sums[l-1]);
+            m--;
+        }
+    }
+    // 求前缀和
+    public static int[] PrefixSum(int[] arr, int n) {
+        int[] sum = new int[n+1];
+        sum[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            sum[i] = sum[i-1] + arr[i-1];
+        }
+        return sum;
+    }
+}
