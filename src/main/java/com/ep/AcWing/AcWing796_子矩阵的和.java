05656109d5744394d889213013d5b097e2d2f4fc
dep <1649743146@qq.com>
2023-03-11 10:11:42 +0800

    feat: 796.子矩阵的和

---
+++ src/main/java/com/ep/AcWing/AcWing796_子矩阵的和.java
@@ -0,0 +1,34 @@
+package com.ep.AcWing;
+
+import java.util.Scanner;
+
+/***
+ * @author dep
+ * @version 1.0
+ * @date 2023-03-11 9:31
+ */
+public class AcWing796_子矩阵的和 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        int q = scanner.nextInt();
+        int[][] arr = new int[n+1][m+1];
+        int[][] sums = new int[n+1][m+1];
+        // 求前缀和
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                arr[i][j] = scanner.nextInt();
+                sums[i][j] = sums[i-1][j] + sums[i][j-1] - sums[i-1][j-1] + arr[i][j];
+            }
+        }
+        while (q > 0) {
+            int x1 = scanner.nextInt();
+            int y1 = scanner.nextInt();
+            int x2 = scanner.nextInt();
+            int y2 = scanner.nextInt();
+            System.out.println(sums[x2][y2] - sums[x1-1][y2] - sums[x2][y1-1] + sums[x1-1][y1-1]);
+            q--;
+        }
+    }
+}
---
+++ src/main/java/com/ep/LeetCode_Type/HashTable/exercise5_454_四数相加II.java
@@ -0,0 +1,12 @@
+package com.ep.LeetCode_Type.HashTable;
+
+/***
+ * @author dep
+ * @version 1.0
+ * @date 2023-03-10 10:00
+ */
+public class exercise5_454_四数相加II {
+    public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+        return -5;
+    }
+}
