59cba15b6c9da0d3dd4686976f036c1464bc832e
dep <1649743146@qq.com>
2023-03-16 10:13:45 +0800

    feat: 206反转链表

---
+++ src/main/java/com/ep/LeetCode_Type/DoublePointer/exercise5_206_反转链表.java
@@ -0,0 +1,28 @@
+package com.ep.LeetCode_Type.DoublePointer;
+
+import java.util.List;
+
+/***
+ * @author dep
+ * @version 1.0
+ * @date 2023-03-16 9:56
+ */
+public class exercise5_206_反转链表 {
+      public class ListNode {
+          int val;
+          ListNode next;
+          ListNode() {}
+          ListNode(int val) { this.val = val; }
+          ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+      }
+    public ListNode reverseList(ListNode  head) {
+        ListNode prev = null, curr = head;
+        while (curr != null) {
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        return prev;
+    }
+}
