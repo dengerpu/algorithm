f07b872881e7ff8ea1ee9aaea276460443dae332
dep <1649743146@qq.com>
2023-03-20 09:16:47 +0800

    feat: 删除字符串中的所有相邻重复项

---
+++ src/main/java/com/ep/LeetCode_Type/StackAndQueue/exercise4_1047_删除字符串中的所有相邻重复项.java
@@ -0,0 +1,33 @@
+package com.ep.LeetCode_Type.StackAndQueue;
+
+import java.util.Stack;
+
+/***
+ * @author dep
+ * @version 1.0
+ * @date 2023-03-20 9:07
+ */
+public class exercise4_1047_删除字符串中的所有相邻重复项 {
+    public static String removeDuplicates(String s) {
+        Stack<Character> stack = new Stack<>();
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+          char ch = s.charAt(i);
+          if (!stack.isEmpty() && stack.peek() == ch) {
+              stack.pop();
+          } else {
+              stack.push(ch);
+          }
+        }
+        StringBuffer sb = new StringBuffer();
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+        return sb.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        String str = "aabbddcghhjj";
+        System.out.println(removeDuplicates(str));
+    }
+}
