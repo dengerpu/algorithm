e66d37a3a41e6ace91171cc1dc5321e4dfa58b36
dep <1649743146@qq.com>
2023-03-22 09:55:34 +0800

    feat: 二叉树后序遍历

---
+++ src/main/java/com/ep/LeetCode_Type/Tree/exercise1_145_二叉树的后序遍历.java
@@ -0,0 +1,42 @@
+package com.ep.LeetCode_Type.Tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/***
+ * @author dep
+ * @version 1.0
+ * @date 2023-03-22 9:44
+ */
+public class exercise1_145_二叉树的后序遍历 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        postorder(root,res);
+        return res;
+    }
+    // 后序遍历
+    public void  postorder(TreeNode root,List<Integer> res) {
+        if (root == null) return;
+        postorder(root.left,res);
+        postorder(root.right,res);
+        res.add(root.val);
+    }
+}
