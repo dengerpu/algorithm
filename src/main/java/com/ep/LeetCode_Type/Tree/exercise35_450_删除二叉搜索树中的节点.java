f118e49255a62046d1b7e2fd53fce183d3426bee
dep <1649743146@qq.com>
2023-04-18 09:45:50 +0800

    feat: 删除二叉搜索树中的节点

--- src/main/java/com/ep/LeetCode_Type/Tree/exercise35_450_删除二叉搜索树中的节点.java
+++ src/main/java/com/ep/LeetCode_Type/Tree/exercise35_450_删除二叉搜索树中的节点.java
@@ -25,7 +25,7 @@ public class exercise35_450_删除二叉搜索树中的节点 {
         }
     }
 
-    public TreeNode deleteNode(TreeNode root, int key) {
+    public TreeNode deleteNode1(TreeNode root, int key) {
         if (root == null) return null;
         root = delete(root, key);
         return root;
@@ -52,5 +52,37 @@ public class exercise35_450_删除二叉搜索树中的节点 {
             curr.right = delete(curr.right, temp.val);
         }
         return curr;
+    }
+
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) return root; // 第一种情况，没找到删除的节点，遍历到空节点值
+        if (root.val == key) {
+            // 第二种情况: 左右孩子都为空（叶子结点），直接删除结点
+            if (root.left == null && root.right == null) {
+                return null;
+            }
+            // 第三种情况：左孩子为空，右孩子不为空，删除节点，右孩子补位，返回右孩子为根节点
+            else if (root.left == null) {
+                return root.right;
+            }
+            // 第四种情况：其右孩子为空，左孩子不为空，删除节点，左孩子补位，返回左孩子为根节点
+            else if (root.right == null) {
+                return root.left;
+            }
+            // 第五种情况：左右孩子节点都不为空，则将删除节点的左子树放到删除节点的右子树的最左面节点的左孩子的位置
+            // 并返回删除节点右孩子为新的根节点。
+            else {
+                TreeNode cur = root.right; // 找右子树最左面的节点
+                while (cur.left != null) {
+                    cur = cur.left;
+                }
+                cur.left = root.left; // 把要删除的节点（root）左子树放在cur的左子树的位置
+                root = root.right; // 返回旧root作为新root
+                return root;
+            }
+        }
+        if (root.val > key) root.left = deleteNode(root.left, key);
+        if (root.val < key) root.right = deleteNode(root.right, key);
+        return root;
     }
 }
