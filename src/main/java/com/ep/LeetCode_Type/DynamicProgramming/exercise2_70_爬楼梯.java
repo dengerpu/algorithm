b467200a53cf4f248b162be28c6c330e15be1c8f
dep <1649743146@qq.com>
2023-05-09 09:26:12 +0800

    feat: 爬楼梯(简化空间复杂度)

--- src/main/java/com/ep/LeetCode_Type/DynamicProgramming/exercise2_70_爬楼梯.java
+++ src/main/java/com/ep/LeetCode_Type/DynamicProgramming/exercise2_70_爬楼梯.java
@@ -17,4 +17,18 @@ public class exercise2_70_爬楼梯 {
         }
         return dp[n];
     }
+
+    // 简化空间复杂度
+    public int climbStairs2(int n) {
+        if (n == 1) return n;
+        int[] dp = new int[3];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[2] = dp[0] + dp[1];
+            dp[0] = dp[1];
+            dp[1] = dp[2];
+        }
+        return dp[2];
+    }
 }
